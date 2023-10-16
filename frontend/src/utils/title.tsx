cb802daeaeebc007e3011ebdb0bcb0cd8f063367
Justin Farnsworth <farnswj1@tcnj.edu>
2023-10-15 20:53:00 -0400

    explicit type declaration

--- frontend/src/utils/title.tsx
+++ frontend/src/utils/title.tsx
@@ -1,4 +1,4 @@
-export const setTitle = (text?: string) => {
+export const setTitle = (text?: string): void => {
   const title = 'Divorce Predictor';
   document.title = text ? `${text} - ${title}` : title;
 };
