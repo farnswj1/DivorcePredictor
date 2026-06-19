8d0e7c02bde31d011656dac947f3dec78ac0e231
Justin Farnsworth <fjj12697@gmail.com>
2026-06-19 19:56:19 -0400

    redirect from 404 page

--- frontend/src/pages/PageNotFoundPage/PageNotFoundPage.tsx
+++ frontend/src/pages/PageNotFoundPage/PageNotFoundPage.tsx
@@ -1,16 +1,22 @@
-import { type FC } from 'react';
+import { useEffect, type FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ButtonLink, Section } from '~/components';
 import { setTitle } from '~/utils';
 
 const PageNotFoundPage: FC = () => {
   setTitle('Page Not Found');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => navigate('/', { replace: true }), 3000);
+  }, [navigate]);
 
   return (
     <Section
       title="Page Not Found"
-      description="It appears you are lost. Click the button below."
+      description="It appears you are lost. Wait to be redirected or click the button below."
       content={
-        <ButtonLink to="/">
+        <ButtonLink to="/" replace>
           Home
         </ButtonLink>
       }
